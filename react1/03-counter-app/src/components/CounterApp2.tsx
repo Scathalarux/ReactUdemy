5c111abccc2a89f658f2e07b03675b34035b2557
Lara CV <lara.cv@tutamail.com>
2025-12-23 18:56:47 +0100

    Finalización useState

--- react1/03-counter-app/src/components/CounterApp2.tsx
+++ react1/03-counter-app/src/components/CounterApp2.tsx
@@ -4,18 +4,31 @@ type CounterApp2Props = {
   value?: number;
 };
 
-export function CounterApp2({ value = 0 }: CounterApp2Props) {
+export function CounterApp2({ value = 10 }: CounterApp2Props) {
+  //Cuando cambia el estado, el componente se vuelve a ejecutar
   const [contador, setContador] = useState<number>(value);
-  const handleClick = () => {
+
+  
+  const handleAdd = () => {
     const newContador = contador + 1;
     setContador(newContador);
+    //setContador(c=> c+1)
+  };
+  const handleSubtract = () => {
+    const newContador = contador - 1;
+    setContador(newContador);
+  };
+  const handleReset = () => {
+    setContador(value);
   };
 
   return (
     <>
       <h1>Counter App</h1>
       <h2> {contador} </h2>
-      <button onClick={handleClick}> +1 </button>
+      <button onClick={handleAdd}> +1 </button>
+      <button onClick={handleSubtract}> -1 </button>
+      <button onClick={handleReset}> Reset </button>
     </>
   );
 }
